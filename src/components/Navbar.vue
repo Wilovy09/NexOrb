5f51d73252d173f991ca7cb09bac3e1cae55fa9f
Wilovy09 <SrWilovy09@gmail.com>
2024-05-18 22:26:35 -0600

    Actualización del enlace de GitHub en la barra de navegación

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,6 +6,6 @@
     <nav class="flex justify-between mx-4 text-white text-2xl">
         <a href="/">NexOrb</a>
 
-        <github-button class="mt-1" href="https://github.com/buttons/github-buttons" data-color-scheme="no-preference: light; light: dark_high_contrast; dark: dark_high_contrast;" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star buttons/github-buttons on GitHub">Star</github-button>
+        <github-button class="mt-1" href="https://github.com/Wilovy09/NexOrb" data-color-scheme="no-preference: light; light: dark_high_contrast; dark: dark_high_contrast;" data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star buttons/github-buttons on GitHub">Star</github-button>
     </nav>
 </template>
